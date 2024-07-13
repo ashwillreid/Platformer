47fea51276159c089e54a9f4a7cf31f46de5c5fe
Reid Ashwill <reidashwill@gmail.com>
2024-07-13 09:12:10 -0700

    move state machine

--- scripts/player/player_fsm.gd
+++ scripts/player/player_fsm.gd
@@ -9,21 +9,23 @@ func _ready():
 	add_state('shooting')
 	add_state('walking')
 	print('states?', states)
+	set_state('idle')
 	
 func _state_logic(delta):
-	#if not parent.is_on_floor():
-			#parent.velocity.y += gravity * delta
+	print('in thurrr')
+	if not parent.is_on_floor():
+			parent.velocity.y += gravity * delta
 
 	match state:
 		states.idle:
 			parent.animated_sprite_2d.play("idle")
 	
-	parent.move_and_slide()
-func _get_transition(delta):
+	#parent.move_and_slide()
+func _get_transition(_delta):
 	return null
 
-func _enter_state(new_state, old_state):
+func _enter_state(_new_state, _old_state):
 	pass
 
-func _exit_state(old_state, new_state):
+func _exit_state(_old_state, _new_state):
 	pass
